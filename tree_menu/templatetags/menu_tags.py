87c9d3f73d11c219c0c8c5a0e514de5f68b83a1f
odhako <odhako@gmail.com>
2023-01-06 12:13:15 +0400

    Make active link <b>bold</b>

--- tree_menu/templatetags/menu_tags.py
+++ tree_menu/templatetags/menu_tags.py
@@ -7,36 +7,49 @@ from tree_menu.models import Menu
 register = template.Library()
 
 
-def draw_node_children(node):
+def draw_node_children(node, current_url):
     children = node.children.all()
     if not children:
         return ''
     html = '<ul>'
     for child in children:
+        name_with_state = child.name
+
+        # Make active link bold
+        if child.url == current_url:
+            name_with_state = f'<b>{child.name}</b>'
+
         html += '<li>'
-        html += f'<a href="{child.url}">{child.name}</a>'
+        html += f'<a href="{child.url}">{name_with_state}</a>'
 
         # Recursive walk
-        html += draw_node_children(child)
+        html += draw_node_children(child, current_url)
         html += '</li>'
     html += '</ul>'
     return html
 
 
-@register.simple_tag
-def draw_menu(menu_name):
+@register.simple_tag(takes_context=True)
+def draw_menu(context, menu_name):
 
     menu = Menu.objects.get(name=menu_name)
     root_nodes = menu.nodes.filter(parent=None)
+    current_url = context.request.path
 
     html = '<ul>'
 
     for root_node in root_nodes:
+        name_with_state = root_node.name
+
+        # Make active link bold
+        if root_node.url == current_url:
+            name_with_state = f'<b>{root_node.name}</b>'
+
         html += '<li>'
-        html += f'<a href="{root_node.url}">{root_node.name}</a>'
+        html += f'<a href="{root_node.url}">{name_with_state}</a>'
 
         # Recursive walk
-        html += draw_node_children(root_node)
+        html += draw_node_children(root_node, current_url)
 
         html += '</li>'
 
